bab6a9f27dafa71ab845f847684f754d19619d84
BruceSniper <270521009@qq.com>
2020-08-16 14:08:44 +0800

    修改选择排序比较大小顺序

--- Sort/01.selectsort.go
+++ Sort/01.selectsort.go
@@ -54,7 +54,7 @@ func SelectSort(arr []int) []int {
 		for i := 0; i < length-1; i++ { //只剩一个元素，不需要挑选
 			min := i                          //标记索引
 			for j := i + 1; j < length; j++ { //每次选出一个极小值
-				if arr[min] < arr[j] {
+				if arr[min] > arr[j] {
 					min = j //保存极小值的索引
 				}
 			}
