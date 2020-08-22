45952f375e15dbc4feb639bd9c1e31c5f36d6e3a
BruceSniper <270521009@qq.com>
2020-08-22 17:47:58 +0800

    双向链表实现

--- LinkList/LinkList.go
+++ LinkList/LinkList.go
@@ -1,88 +1,52 @@
 package LinkList
 
+import "fmt"
+
 type Node struct {
-	data  interface{}
-	pNext *Node
+	Data      int
+	NextPoint *Node
+	PrePoint  *Node
 }
 
-type LinkList struct {
-	head   *Node
-	length uint
+type LinkedList struct {
+	head    *Node
+	current *Node
+	tail    *Node
 }
 
-func NewList() *Node {
-	return &Node{} //返回一个节点指针
-}
-
-func Find(n *Node, data interface{}) *Node {
-	tmp := n
-	if tmp.data != data {
-		tmp = tmp.pNext
+func CreatLinkedList() {
+	data := []int{1, 21, 31, 51, 62, 2, 3, 42, 33, 12, 12}
+	link := LinkedList{}
+	var currentNode *Node
+	for i := 0; i < len(data); i++ {
+		currentNode = new(Node)
+		currentNode.Data = data[i]
+		insertNode(&link, currentNode)
 	}
-	return tmp
+	showLinkedList(link)
 }
 
-// 链表是否为空
-func IsEmpty(n *Node) bool {
-	return n.pNext == nil
-}
-
-// 是否是最后一个节点
-func IsLast(n *Node) bool {
-	return n.pNext == nil
-}
-
-// 插入节点:在指定节点插入节点
-/**
-position:指定的节点位置
-*/
-func Insert(data interface{}, position *Node) {
-	//新建一个节点
-	tmpNode := new(Node)
-	// 给新建的节点的值域赋值为传入的data值
-	tmpNode.data = data
-	// 新建的节点的next指针指向的是指定节点position的next
-	tmpNode.pNext = position.pNext
-	// 指定节点position的后继节点变成了新建的节点
-	position.pNext = tmpNode
-}
-
-// 查找指定节点的前一个节点
-func FindPrevious(data interface{}, n *Node) *Node {
-	tmp := n
-	for tmp.pNext != nil && tmp.pNext.data != data {
-		tmp = tmp.pNext
+func showLinkedList(link LinkedList) {
+	var currentNode *Node
+	currentNode = link.head
+	for {
+		fmt.Println("Node:", currentNode.Data)
+		if currentNode.NextPoint == nil {
+			break
+		} else {
+			currentNode = currentNode.NextPoint
+		}
 	}
-	return tmp
 }
 
-// 删除节点
-func Delete(data interface{}, n *Node) {
-
+func insertNode(link *LinkedList, node *Node) {
+	if link.head == nil {
+		link.head = node
+		link.tail = node
+		link.current = node
+	} else {
+		link.tail.NextPoint = node
+		node.PrePoint = link.tail
+		link.tail = node
+	}
 }
-
-//func (n *Node) Push(data interface{}) {
-//	newnode := &Node{data: data}
-//	newnode.pNext = n.pNext
-//	n.pNext = newnode //头部插入
-//}
-//
-////栈操作方向，
-//func (n *Node) Pop() interface{} {
-//	if n.IsEmpty() == true {
-//		return nil
-//	}
-//	value := n.pNext.data   //要弹出的数据
-//	n.pNext = n.pNext.pNext //删除
-//	return value
-//}
-//
-//func (n *Node) Length() int {
-//	pnext := n
-//	length := 0
-//	for pnext.pNext != nil {
-//		pnext = pnext.pNext //节点的循环跳跃
-//		length++            //追加
-//	}
-//	return length //返回长度
-//}
