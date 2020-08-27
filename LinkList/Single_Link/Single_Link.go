64bd57b32e0c9f18d4f8c9392acb67084b9010cf
BruceSniper <270521009@qq.com>
2020-08-27 22:11:33 +0800

    实现单链表几项功能

--- LinkList/Single_Link/Single_Link.go
+++ LinkList/Single_Link/Single_Link.go
@@ -1,17 +1,24 @@
 package Single_Link
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //单链表的接口
 type SingleLink interface {
 	//增删查改
-	GetFirstNode() *SingleLinkNode            //抓取头部节点
-	InsertNodeFront(node *SingleLinkNode)     //头部插入
-	InsertNodeBack(node *SingleLinkNode)      //尾部插入
+	GetFirstNode() *SingleLinkNode        //抓取头部节点
+	InsertNodeFront(node *SingleLinkNode) //头部插入
+	InsertNodeBack(node *SingleLinkNode)  //尾部插入
+
+	InsertNodeValueFront(dest interface{}, node *SingleLinkNode) bool //根据dest值搜索，在该节点之前插入node
+	InsertNodeValueBack(dest interface{}, node *SingleLinkNode) bool  //根据dest值搜索，在该节点之后插入node
+
 	GetNodeAtIndex(index int) *SingleLinkNode //根据索引抓取指定位置的节点
 	DeleteNode(dest *SingleLinkNode) bool     //删除一个节点，判断是否删除成功
 	DeleteAtIndex(index int) bool             //根据索引值删除节点，判断是否成功
 	String() string                           //返回链表字符串
+	GetMid() *SingleLinkNode                  //求链表的中间位置
 }
 
 //单链表的结构
@@ -70,4 +77,104 @@ func (list *SingleLinkList) String() string {
 	}
 	listString += fmt.Sprintf("nil")
 	return listString //打印链表字符串
+}
+
+func (list *SingleLinkList) InsertNodeValueFront(dest interface{}, node *SingleLinkNode) bool {
+	phead := list.head
+	isfind := false
+	for phead.pNext != nil {
+		if phead.pNext.value == dest { //找到
+			isfind = true
+			break
+		}
+		phead = phead.pNext
+	}
+	if isfind {
+		node.pNext = phead.pNext
+		phead.pNext = node
+		list.length++
+		return true
+	} else {
+		return false
+	}
+}
+
+func (list *SingleLinkList) InsertNodeValueBack(dest interface{}, node *SingleLinkNode) bool {
+	phead := list.head
+	isfind := false
+	for phead != nil {
+		if phead.value == dest { //找到
+			isfind = true
+			break
+		}
+		phead = phead.pNext
+	}
+	if isfind {
+		node.pNext = phead.pNext
+		phead.pNext = node
+		list.length++
+		return true
+	} else {
+		return false
+	}
+
+}
+
+func (list *SingleLinkList) GetNodeAtIndex(index int) *SingleLinkNode {
+	if index > list.length-1 || index < 0 {
+		return nil
+	} else {
+		phead := list.head
+		for index > -1 {
+			phead = phead.pNext //向后循环
+			index--             //向后循环的过程
+		}
+		return phead //返回的是地址
+	}
+}
+
+func (list *SingleLinkList) DeleteNode(dest *SingleLinkNode) bool {
+	if dest == nil {
+		return false
+	}
+	phead := list.head
+	for phead.pNext != nil && phead.pNext != dest {
+		phead = phead.pNext //循环下去
+	}
+	if phead.pNext == dest {
+		phead.pNext = phead.pNext.pNext
+		list.length--
+		return true
+	} else {
+		return false
+	}
+}
+
+func (list *SingleLinkList) DeleteAtIndex(index int) bool {
+	if index > list.length-1 || index < 0 {
+		return false
+	} else {
+		phead := list.head
+		for index > 0 {
+			phead = phead.pNext //向后循环
+			index--             //向后循环的过程
+		}
+		phead.pNext = phead.pNext.pNext
+		list.length--
+		return true
+	}
+}
+
+func (list *SingleLinkList) GetMid() *SingleLinkNode {
+	if list.head.pNext == nil {
+		return nil
+	} else {
+		phead1 := list.head
+		phead2 := list.head
+		for phead2 != nil && phead2.pNext != nil {
+			phead1 = phead1.pNext
+			phead2 = phead2.pNext.pNext
+		}
+		return phead1 //中间节点
+	}
 }
