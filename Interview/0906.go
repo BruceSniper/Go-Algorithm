65a0cb36b79819d83b242cae2084730c6ce513a8
BruceSniper <270521009@qq.com>
2020-09-06 21:52:56 +0800

    腾讯面试题0906初稿

---
+++ Interview/0906.go
@@ -0,0 +1,16 @@
+package main
+
+import "strings"
+
+func Test(str1, str2 string, len1, len2 int) string {
+	if len1 > len2 {
+
+	}
+}
+
+func main() {
+	var length1, length2 int
+	var str1_old, str2_old string
+	str1_new := strings.Replace(str1_old, " ", "", -1)
+	str2_new := strings.Replace(str2_old, " ", "", -1)
+}
