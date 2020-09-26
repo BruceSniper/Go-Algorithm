20e4ecead3790c855d5b47e5ee2a8cec028b0b19
BruceSniper <270521009@qq.com>
2020-09-26 16:18:05 +0800

    第98题，递归法验证二叉搜索树

---
+++ Leetcode/098验证二叉搜索树.go
@@ -0,0 +1,29 @@
+package Leetcode
+
+import "math"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isValidBST(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isBST(root, math.MinInt64, math.MaxInt64)
+}
+
+func isBST(root *TreeNode, min, max int) bool {
+	if root == nil {
+		return true
+	}
+	if min >= root.Val || max <= root.Val {
+		return false
+	}
+	return isBST(root.Left, min, root.Val) && isBST(root.Right, root.Val, max)
+}
